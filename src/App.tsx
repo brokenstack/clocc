880273c901e40487c29e7c7b558ba2d5b7cdb2ac
alok8bb <alok8bb@gmail.com>
2023-07-22 19:15:37 +0530

    implement a basic clock with dark/light mode
    Signed-off-by: alok8bb <alok8bb@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { CornersOut, CornersIn, MoonIcon, SunIcon } from "./assets/Icon";
+
+function App() {
+    const [storedTheme, setStoredTheme] = useState(
+        localStorage.getItem("theme") ||
+            (window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "dark"
+                : "light")
+    );
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", storedTheme);
+    }, [storedTheme]);
+
+    useEffect(() => {
+        const timerID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timerID);
+        };
+    }, []);
+
+    const switchTheme = () => {
+        setStoredTheme(storedTheme == "light" ? "dark" : "light");
+    };
+
+    const [fullScreen, setFullScreen] = useState(
+        Boolean(document.fullscreenElement)
+    );
+
+    const toggleFullScreen = async () => {
+        try {
+            await (document.fullscreenElement
+                ? document.exitFullscreen()
+                : document.documentElement.requestFullscreen());
+        } finally {
+            setFullScreen(Boolean(document.fullscreenElement));
+        }
+    };
+
+    return (
+        <>
+            <div className="container">
+                <div>
+                    <div className="menu-container">
+                        <div onClick={() => switchTheme()}>
+                            {storedTheme == "light" ? (
+                                <MoonIcon size={31} />
+                            ) : (
+                                <SunIcon size={32} />
+                            )}
+                        </div>
+
+                        <div onClick={() => toggleFullScreen()}>
+                            {fullScreen ? (
+                                <CornersIn size={32} />
+                            ) : (
+                                <CornersOut size={32} />
+                            )}
+                        </div>
+                    </div>
+                </div>
+
+                <p
+                    id="time-text"
+                    className="noselect"
+                    onDoubleClick={() => toggleFullScreen()}
+                >
+                    {`${
+                        time.getHours() % 12 ? time.getHours() % 12 : 12
+                    }:${time.getMinutes()}`}
+                    <span id="am-pm-text">
+                        {time.getHours() >= 12 ? "pm" : "am"}
+                    </span>
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+declare global {
+    interface Document {
+        mozCancelFullScreen?: () => Promise<void>;
+        msExitFullscreen?: () => Promise<void>;
+        webkitExitFullscreen?: () => Promise<void>;
+        mozFullScreenElement?: Element;
+        msFullscreenElement?: Element;
+        webkitFullScreenElement?: Element;
+    }
+
+    interface HTMLElement {
+        msRequestFullscreen?: () => Promise<void>;
+        mozRequestFullscreen?: () => Promise<void>;
+        webkitRequestFullscreen?: () => Promise<void>;
+    }
+}
